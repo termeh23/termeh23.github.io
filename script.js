92770ad34d52d64d82ec5797213ed91cc95f81c3
termeh23 <122713491+termeh23@users.noreply.github.com>
2023-01-15 00:10:24 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+// Dark Mode / light mode
+let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
+
+var form = document.getElementById('form')
+form.addEventListener('submit', function(event){
+    event.preventDefault() // prevents the form from autosubmitting 
+
+    var username = document.getElementById('username').value
+
+    console.log(username)
+
+    var email = document.getElementById('email').value
+
+    console.log(email)
+
+    var text = document.getElementById('message').value
+
+    console.log(message)
+});    
+
+                                    
